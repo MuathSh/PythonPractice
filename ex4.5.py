cc2cb867ef11f4c2003c1bec008e8d2d88f68a62
MuathSh <280415435+MuathSh@users.noreply.github.com>
2026-08-25 12:19:23 +0300

    renaming

--- ex4.5.py
+++ ex4.5.py
@@ -1,6 +1,10 @@
-#Practice using pass
-class ExampleForPASS:
-    pass
+#example for using Break and Continue
+for m in range(1, 10):
 
-def ExampleForPASS():
-    pass
+        if m == 8:
+                break
+        
+        if m % 2 !=0:
+             continue
+        print(f"{m}")
+            
